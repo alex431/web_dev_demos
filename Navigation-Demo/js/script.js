1b201d3a24124f0f3f83b487ed034d1cdbf69be8
Alex Hinkle <alex.hinkle431@gmail.com>
2023-09-01 20:05:31 -0400

    css/style.css

--- Navigation-Demo/js/script.js
+++ Navigation-Demo/js/script.js
@@ -1,10 +1,11 @@
 const hamburger = document.querySelector(".hamburger")
 const mynav = document.querySelector("#nav1")
+
+
 hamburger.addEventListener("click", myFunction);
- 
+
 function myFunction() {
-    //console.log("hello world");
-    mynav.classList.toggle('active')
+    mynav.classList.toggle('active');
   }
 
   
