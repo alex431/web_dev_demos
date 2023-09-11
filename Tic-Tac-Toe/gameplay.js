52ff24b312c9e95d95e61b9393d31a611a6b6986
Alex Hinkle <alex.hinkle431@gmail.com>
2023-09-11 11:30:47 -0400

    Implemented Reset Button and Responsiveness

--- Tic-Tac-Toe/gameplay.js
+++ Tic-Tac-Toe/gameplay.js
@@ -24,8 +24,6 @@ function move_player(i)
     return;
   }
 
-  // board_spaces.forEach(cell => cell.classList.remove("player-X","player-O"));
-  
   game_board[i] = current_player;
   board_spaces[i].textContent = current_player;
   board_spaces[i].classList.add(`player-${current_player}`);
@@ -47,6 +45,8 @@ function check_win()
     [0, 4, 8], [2, 4, 6]
   ];
    
+  let win_found =false;
+  
   for (let combo of winning_combos)
   {
     const [a, b, c] = combo;
@@ -54,110 +54,34 @@ function check_win()
     {
       meaning.textContent = `Player ${current_player} wins!`;
       game_on = false;
+      win_found = true;
+      break;
     }
   }
 
-  if(!game_board.includes(""))
+  if(!win_found && !game_board.includes(""))
   {
     meaning.textContent = "It's a draw!";
     game_on = false;
   }
 }
 
-// function winner()
-// {
-//     for(let i = 0; i < 3; i++)
-//     {
-//         if(space[i * 3] === space[i * 3 + 1]  === space[i * 3 + 2] && space[i * 3] !== " ")
-//         {
-//             return true
-//         } 
-//     }
+const resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", reset_board);
 
-//     for (let i = 0; i < 3; i++) 
-//     {
-//         if (space[i] === space[i + 3] === space[i + 6] && space[i] !== " ") 
-//         {
-//             return true;
-//         }
-//     }
+function reset_board() 
+{
+  game_board = ["", "", "", "", "", "", "", "", ""];
+  game_on = true;
+  current_player = "X";
+  clear_board();
+  meaning.textContent = `Player ${current_player}'s turn`;
+}
 
-//       // Check for a diagonal win.
-//     if (space[0] === space[4] === space[8] && space[0] !== " ")
-//     {
-//         return true;
-//     }
-
-//     if (space[2] === space[4] === space[6] && space[2] !== " ") 
-//     {
-//     return true;
-//     }
-
-//   return false;
-// }
-
-
-
-
-// function checkWinner() {
-//   // Check for a row win.
-//   for (var i = 0; i < 3; i++) 
-        //{
-//     if (cells[i * 3].innerHTML === cells[i * 3 + 1].innerHTML === cells[i * 3 + 2].innerHTML && cells[i * 3].innerHTML !== "") 
-      // {
-//       return true;
-//     }
-//   }
-
-//   // Check for a column win.
-//   for (var i = 0; i < 3; i++)
-// {
-//     if (cells[i].innerHTML === cells[i + 3].innerHTML === cells[i + 6].innerHTML && cells[i].innerHTML !== "") {
-//       return true;
-//     }
-//   }
-
-//   // Check for a diagonal win.
-//   if (cells[0].innerHTML === cells[4].innerHTML === cells[8].innerHTML && cells[0].innerHTML !== "") {
-//     return true;
-//   }
-
-//   if (cells[2].innerHTML === cells[4].innerHTML === cells[6].innerHTML && cells[2].innerHTML !== "") {
-//     return true;
-//   }
-
-//   return false;
-//}
-
-
-
-// let surface = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-// const outcomes = [ 
-//     [0, 1, 2], [3, 4, 5], [6, 7, 8],
-//     [0, 3, 6], [1, 4, 7], [2, 5, 8],
-//     [0, 4, 8], [2, 4, 6]
-// ]
-
-// const paper = document.getElementById("paper");
-// const currentState = document.getElementById("state")
-
-// function makeMove(cellIndex) {
-//     if (gameBoard[cellIndex] === '' && !checkWinner()) 
-//     {
-//         gameBoard[cellIndex] = currentPlayer;
-//         board.children[cellIndex].textContent = currentPlayer;
-//         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-//         status.textContent = `Player ${currentPlayer}'s turn`;
-//         checkWinner();
-//     }
-// }
-
-// function checkWinner() {
-//     for (const combination of winningCombinations) {
-//         const [a, b, c] = combination;
-//         if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-//             status.textContent = `Player ${gameBoard[a]} wins!`;
-//             return true;
-//         }
-//     }
-// }
+function clear_board()
+{
+  board_spaces.forEach((cell) => {
+    cell.textContent = ""; 
+    cell.classList.remove("player-X", "player-O");
+  });
+}
