90e8a7d0b824c95ac8cacec0b440eed8c63f0a54
Alex Hinkle <alex.hinkle431@gmail.com>
2023-09-14 11:28:20 -0400

    Added Comments

--- Navigation-Demo/js/active.js
+++ Navigation-Demo/js/active.js
@@ -1,9 +1,11 @@
+// Get the hamburger menu and navigation elements by their CSS selectors
 const hamburger = document.querySelector(".hamburger")
 const mynav = document.querySelector("#nav1")
 
-
+// Add a click event listener to the hamburger menu
 hamburger.addEventListener("click", myFunction);
 
+// Function to toggle the 'active' class on the navigation element
 function myFunction() {
     mynav.classList.toggle('active');
   }
